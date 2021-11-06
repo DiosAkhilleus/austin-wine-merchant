4fb5a38e1899cb6076ce32d6e23af7031483813e
DiosAkhilleus <abertin1809@gmail.com>
2021-11-06 15:41:43 -0500

    modified routing

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,24 +1,25 @@
 import React from 'react';
-import logo from './logo.svg';
+import NavBar from './Components/NavBar';
+import headerSVG from './Resources/header_title.svg';
+import { Routes, Route } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
+import FineWine from './Components/FineWine';
+import Spirits from './Components/Spirits';
+import Home from './Components/Home';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className='header'>
+        <img className='header-svg' src={headerSVG} alt="Austin Wine Merchant" />
       </header>
+      <NavBar />
+      <Routes>
+        <Route path='/wine' element={<FineWine />}/>
+        <Route path='/spirits' element={<Spirits />}/>
+        <Route path='/home' element={<Home />}/>
+      </Routes>
     </div>
   );
 }
